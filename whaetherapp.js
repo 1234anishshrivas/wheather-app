f0de047d59469cd49bde2db9c928e9d27f0bff0b
1234anishshrivas <143332860+1234anishshrivas@users.noreply.github.com>
2023-11-22 11:03:55 +0530

    Add files via upload

---
+++ whaetherapp.js
@@ -0,0 +1,51 @@
+const apiKey = " "
+const apiUrl = " "
+
+    const searchBox = document.querySelector('.search input');
+    const searchBtn = document.querySelector('.search button'); 
+    const weatherIcon = document.querySelector('.weather-icon');
+
+    async function checkWeather(city)
+    {
+        const response = await fetch(apiUrl + city + '&appid=${apiKey}')
+        var data = await response.json()
+
+        document.querySelector('.weather').style.display = "flex"
+
+        document.querySelector('.city').innerHTML = data.name;
+        document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + "c";
+        document.querySelector('.humidity').innerHTML = data.main.humidity + "%";
+        document.querySelector('.wind').innerHTML = data.wind.speed + "km/h";
+
+        if(data.weather[0].main == "Clouds")
+        {
+            weatherIcon.src = "clouds.png"
+        }
+        else if(data.weather[0].main == "Rain")
+        {
+            weatherIcon.src = "rain.png"
+        }
+        else if(data.weather[0].main == "Drizzle")
+        {
+            weatherIcon.src = "drizzle.png"
+        }
+        else if(data.weather[0].main == "Mist")
+        {
+            weatherIcon.src = "mist.png"
+        }
+        else if(data.weather[0].main == "Clear")
+        {
+            weatherIcon.src = "clear.png"
+        }
+        else if(data.weather[0].main == "Snow")
+        {
+            weatherIcon.src = "snow.png"
+        }
+        
+    }
+
+    searchBtn.addEventListener("click", () => 
+    {
+        checkWeather(searchBox.value);
+    })
+
